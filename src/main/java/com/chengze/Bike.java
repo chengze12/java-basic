c48fd298abbcb5c1cd4695044f9f7f6113f275f8
chengze12 <chengzewang612@gmail.com>
2019-04-06 11:23:34 -0400

    add bike class

---
+++ src/main/java/com/chengze/Bike.java
@@ -0,0 +1,27 @@
+package com.chengze;
+
+public class Bike {
+    public int wheelNum;
+    public String seatShape;
+
+    public Bike(){
+        wheelNum=1;
+    }
+    public Bike(int wn){
+        this.wheelNum= wn;
+    }
+    public void move(){
+
+    }
+    public String toString(){
+        return String.valueOf(this.wheelNum);
+    }
+
+    public static void main(String[] args){
+        Bike b= new Bike();
+        Bike b1= new Bike(  2);
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println("I am awesome!");
+    }
+}
